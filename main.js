4b81f7fb77f8d7d408019d2377ecb31908e01537
KENNETH MICHAEL <kennethmichael11@gmail.com>
2017-09-26 16:58:02 +0100

    Task on OOP

---
+++ main.js
@@ -0,0 +1,27 @@
+const Animal = function(sound) {
+    this.sound = sound;
+};
+
+Animal.prototype.makeSound = function() {
+    console.log(this.sound);
+};
+
+const Dog = function(name, breed) {
+    this.name = name;
+    this.breed = breed;
+}
+
+Dog.prototype = new Animal('woof');
+
+const Cat = function(name, breed) {
+    this.name = name;
+    this.breed = breed;
+};
+
+Cat.prototype = new Animal('meow');
+
+const dog = new Dog('Fido', 'Collie');
+const cat = new Cat('Oliver', 'Siamese');
+
+dog.makeSound();
+cat.makeSound();
